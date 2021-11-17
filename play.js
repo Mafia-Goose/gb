359f9a603bae24e3884472a10d08bb326038505e
Mafia-Goose <78287101+Mafia-Goose@users.noreply.github.com>
2021-11-17 18:46:18 -0500

    Create play.js

---
+++ play.js
@@ -0,0 +1,56 @@
+var verycringe = "1";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var password = "glizzy";
+function passcheck() {
+if(document.getElementById('pass1').value != password) {
+alert('Wrong Password, Try Again.');
+return false;
+}
+if(document.getElementById('passi').value == password) {
+alert('Correct Password. click ok to enter Webpage.');
+}
+)
